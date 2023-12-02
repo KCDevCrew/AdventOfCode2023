4cbd164ccbff4bf09d76a004856b548036ba30c3
delacruznic <Nicolas.Delacruz@choosemylo.com>
2023-12-01 20:18:02 -0600

    add: files back

---
+++ delacruznic/README.md
@@ -0,0 +1,4 @@
+## Setup Instructions
+1. [Download](https://dotnet.microsoft.com/en-us/download) and install `.NET`
+2. In a terminal change directory to solution of day you want to run
+3. Run application `dotnet run`
---
+++ delacruznic/day1/Program.cs
@@ -0,0 +1,11 @@
+﻿using day1;
+class TestClass
+{
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Advent of code Day 1");
+      var trebuchet = new Trebuchet("input.txt");
+      int calibration = trebuchet.getCalibration();
+      Console.WriteLine($"Calculated calibration for trebuchet: {calibration}");
+    }
+}
