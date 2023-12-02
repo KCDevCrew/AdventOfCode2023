bf279f22917f3b2d0feb815d3617464f6701c770
delacruznic <Nicolas.Delacruz@choosemylo.com>
2023-12-01 20:15:28 -0600

    updated: log message

--- delacruznic/day1/Program.cs
+++ delacruznic/day1/Program.cs
@@ -6,6 +6,6 @@ class TestClass
       Console.WriteLine("Advent of code Day 1");
       var trebuchet = new Trebuchet("input.txt");
       int calibration = trebuchet.getCalibration();
-      Console.WriteLine($"Part 1: Calculated calibration for trebuchet: {calibration}");
+      Console.WriteLine($"Calculated calibration for trebuchet: {calibration}");
     }
 }
