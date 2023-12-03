236668a220e5a8d5109e4761c2e48c984f232a59
delacruznic <Nicolas.Delacruz@choosemylo.com>
2023-12-03 10:21:03 -0600

    Day 2: Part 1

---
+++ delacruznic/day2/CubeConundrum.cs
@@ -0,0 +1,74 @@
+﻿namespace day2;
+
+using System.IO;
+using System.Text.RegularExpressions;
+
+class CubeConundrum
+{
+  public const int MAX_RED = 12;
+  public const int MAX_GREEN = 13;
+  public const int MAX_BLUE = 14;
+  private string fileName;
+  public CubeConundrum(string fileName)
+  {
+    this.fileName = fileName;
+  }
+
+  public int getEligibleGamesTotal()
+  {
+    int eligibleGamesTotal = 0;
+    try
+    {
+      string path = Directory.GetCurrentDirectory() + "/" + this.fileName;
+      StreamReader sr = new StreamReader(path);
+
+      //Read the first line of text
+      String line = sr.ReadLine();
+
+      //Continue to read until you reach end of file
+      while (line != null)
+      {
+
+        eligibleGamesTotal += this.processGame(line);
+
+        //Read the next line
+        line = sr.ReadLine();
+      }
+      //close the file
+      sr.Close();
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine("Exception: " + e.Message);
+    }
+    return eligibleGamesTotal;
+  }
+
+  /**
+  *
+  */
+  private int processGame(string game)
+  {
+    bool eligibleGame = true;
+    int gameId = Int32.Parse(game[5..game.IndexOf(":")]);
+    string[] parsedGames = game.Substring(game.IndexOf(":") + 1)
+      .Split(new Char [] {',' , ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+    foreach (string parsedGame in parsedGames) {
+      if (parsedGame.Contains("blue") ) {
+        int blueValue = Int32.Parse(Regex.Match(parsedGame, @"\d+").Value);
+        if (blueValue > MAX_BLUE) eligibleGame = false;
+      }
+      else if (parsedGame.Contains("red") ) {
+        int redValue = Int32.Parse(Regex.Match(parsedGame, @"\d+").Value);
+        if (redValue > MAX_RED) eligibleGame = false;
+      }
+      else if (parsedGame.Contains("green") ) {
+        int greenValue = Int32.Parse(Regex.Match(parsedGame, @"\d+").Value);
+        if (greenValue > MAX_GREEN) eligibleGame = false;
+      }
+    }
+
+    return eligibleGame ? gameId : 0;
+  }
+}
---
+++ delacruznic/day2/Program.cs
@@ -0,0 +1,11 @@
+﻿using day2;
+class MainClass
+{
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Advent of code Day 2");
+      var cubeConundrum = new CubeConundrum("input.txt");
+      int total = cubeConundrum.getEligibleGamesTotal();
+      Console.WriteLine($"Total eligible games {total}");
+    }
+}
