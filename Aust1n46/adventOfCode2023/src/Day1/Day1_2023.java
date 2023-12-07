71060cb822c4b5f86d8c3158634b1b803a6d2e4f
Aust1n46 <aust1n46@yahoo.com>
2023-12-07 05:42:18 -0600

    Replace usage of scanner on Day1,2,3,4,6

--- Aust1n46/adventOfCode2023/src/Day1/Day1_2023.java
+++ Aust1n46/adventOfCode2023/src/Day1/Day1_2023.java
@@ -1,101 +1,92 @@
 package Day1;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Day1_2023 {
-	private static final File INPUT = new File("src/Day1/input.txt");
-	private static final Map<String, String> DIGITS = Map.of("one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9");
+	private static final Map<String, String> DIGITS = Map.of("one", "1", "two", "2", "three", "3", "four", "4", "five",
+			"5", "six", "6", "seven", "7", "eight", "8", "nine", "9");
 
-	public static void main(String[] args) {
-		System.out.println(getCalibrationValuesPartOne());
-		System.out.println(getCalibrationValuesPartTwo());
+	public static void main(String[] args) throws IOException {
+		final List<String> input = Files.readAllLines(Path.of("src/Day1/input.txt"));
+		System.out.println(getCalibrationValuesPartOne(input));
+		System.out.println(getCalibrationValuesPartTwo(input));
 	}
 
-	private static int getCalibrationValuesPartOne() {
+	private static int getCalibrationValuesPartOne(final List<String> input) {
 		int sum = 0;
-		try (Scanner scanner = new Scanner(INPUT)) {
-			while (scanner.hasNextLine()) {
-				final String line = scanner.nextLine();
-				String firstDigit = "";
-				String lastDigit = "";
-				for (char c : line.toCharArray()) {
-					if (c > 57) { // not a number
-						continue;
+		for (final String line : input) {
+			String firstDigit = "";
+			String lastDigit = "";
+			for (char c : line.toCharArray()) {
+				if (c > 57) { // not a number
+					continue;
+				}
+				if (firstDigit.isEmpty()) {
+					firstDigit = String.valueOf(c);
+				} else {
+					lastDigit = String.valueOf(c);
+				}
+			}
+			if (lastDigit.isEmpty()) {
+				lastDigit = firstDigit;
+			}
+			sum += Integer.parseInt(firstDigit + lastDigit);
+		}
+		return sum;
+	}
+
+	private static int getCalibrationValuesPartTwo(final List<String> input) {
+		int sum = 0;
+		for (final String line : input) {
+			String firstDigit = "";
+			String lastDigit = "";
+			String word3 = "";
+			String word4 = "";
+			String word5 = "";
+			for (char c : line.toCharArray()) {
+				word3 += c;
+				word4 += c;
+				word5 += c;
+				if (word3.length() > 3) {
+					word3 = word3.substring(1);
+				}
+				if (word4.length() > 4) {
+					word4 = word4.substring(1);
+				}
+				if (word5.length() > 5) {
+					word5 = word5.substring(1);
+				}
+				if (c > 57) { // not a number
+					for (final String s : List.of(word3, word4, word5)) {
+						if (DIGITS.containsKey(s)) {
+							final String digit = DIGITS.get(s);
+							if (firstDigit.isEmpty()) {
+								firstDigit = digit;
+							} else {
+								lastDigit = digit;
+							}
+							break;
+						}
 					}
+				} else {
+					word3 = "";
+					word4 = "";
+					word5 = "";
 					if (firstDigit.isEmpty()) {
 						firstDigit = String.valueOf(c);
 					} else {
 						lastDigit = String.valueOf(c);
 					}
 				}
-				if (lastDigit.isEmpty()) {
-					lastDigit = firstDigit;
-				}
-				sum += Integer.parseInt(firstDigit + lastDigit);
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return sum;
-	}
-
-	private static int getCalibrationValuesPartTwo() {
-		int sum = 0;
-		try (Scanner scanner = new Scanner(INPUT)) {
-			while (scanner.hasNextLine()) {
-				final String line = scanner.nextLine();
-				String firstDigit = "";
-				String lastDigit = "";
-				String word3 = "";
-				String word4 = "";
-				String word5 = "";
-				for (char c : line.toCharArray()) {
-					word3 += c;
-					word4 += c;
-					word5 += c;
-					if (word3.length() > 3) {
-						word3 = word3.substring(1);
-					}
-					if (word4.length() > 4) {
-						word4 = word4.substring(1);
-					}
-					if (word5.length() > 5) {
-						word5 = word5.substring(1);
-					}
-					if (c > 57) { // not a number
-						for (final String s : List.of(word3, word4, word5)) {
-							if (DIGITS.containsKey(s)) {
-								final String digit = DIGITS.get(s);
-								if (firstDigit.isEmpty()) {
-									firstDigit = digit;
-								} else {
-									lastDigit = digit;
-								}
-								break;
-							}
-						}
-					} else {
-						word3 = "";
-						word4 = "";
-						word5 = "";
-						if (firstDigit.isEmpty()) {
-							firstDigit = String.valueOf(c);
-						} else {
-							lastDigit = String.valueOf(c);
-						}
-					}
-				}
-				if (lastDigit.isEmpty()) {
-					lastDigit = firstDigit;
-				}
-				sum += Integer.parseInt(firstDigit + lastDigit);
+			if (lastDigit.isEmpty()) {
+				lastDigit = firstDigit;
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			sum += Integer.parseInt(firstDigit + lastDigit);
 		}
 		return sum;
 	}
--- Aust1n46/adventOfCode2023/src/Day4/Day4_2023.java
+++ Aust1n46/adventOfCode2023/src/Day4/Day4_2023.java
@@ -1,62 +1,51 @@
 package Day4;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day4_2023 {
-	private static final File INPUT = new File("src/Day4/input.txt");
-
-	public static void main(String[] args) {
-		System.out.println(getSumPartOne());
-		System.out.println(getSumPartTwo());
+	public static void main(String[] args) throws IOException {
+		final List<String> input = Files.readAllLines(Path.of("src/Day4/input.txt"));
+		System.out.println(getSumPartOne(input));
+		System.out.println(getSumPartTwo(input));
 	}
 
-	private static int getSumPartOne() {
+	private static int getSumPartOne(final List<String> input) {
 		int sum = 0;
-		try (final Scanner scanner = new Scanner(INPUT)) {
-			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				line = line.substring(line.indexOf(":") + 1).trim();
-				final String winningNumbersString = line.substring(0, line.indexOf("|")).trim();
-				final String ourNumbersString = line.substring(line.indexOf("|") + 1, line.length()).trim();
-				final Set<String> winningNumbers = Stream.of(winningNumbersString.split("\\s+"))
-						.collect(Collectors.toSet());
-				final Set<String> ourNumbers = Stream.of(ourNumbersString.split("\\s+"))
-						.collect(Collectors.toSet());
-				sum += (int) Math.pow(2, ourNumbers.stream().filter(x -> winningNumbers.contains(x)).count() - 1);
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		for (final String line : input) {
+			final Set<String> winningNumbers = Stream
+					.of(line.substring(line.indexOf(":"), line.indexOf("|")).trim().split("\\s+"))
+					.collect(Collectors.toSet());
+			final Set<String> ourNumbers = Stream
+					.of(line.substring(line.indexOf("|") + 1, line.length()).trim().split("\\s+"))
+					.collect(Collectors.toSet());
+			sum += (int) Math.pow(2, ourNumbers.stream().filter(x -> winningNumbers.contains(x)).count() - 1);
 		}
 		return sum;
 	}
-	
-	private static int getSumPartTwo() {
+
+	private static int getSumPartTwo(final List<String> input) {
 		final Map<Integer, Integer> cards = new HashMap<>();
-		try (final Scanner scanner = new Scanner(INPUT)) {
-			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				final int cardNumber = Integer.parseInt(line.substring(5, line.indexOf(":")).trim());
-				cards.putIfAbsent(cardNumber, 1);
-				line = line.substring(line.indexOf(":") + 1).trim();
-				final String winningNumbersString = line.substring(0, line.indexOf("|")).trim();
-				final String ourNumbersString = line.substring(line.indexOf("|") + 1, line.length()).trim();
-				final Set<String> winningNumbers = Stream.of(winningNumbersString.split("\\s+"))
-						.collect(Collectors.toSet());
-				final Set<String> ourNumbers = Stream.of(ourNumbersString.split("\\s+"))
-						.collect(Collectors.toSet());
-				final int numMatches = (int) ourNumbers.stream().filter(x -> winningNumbers.contains(x)).count();
-				IntStream.range(cardNumber + 1, cardNumber + numMatches + 1).forEach(x -> cards.put(x, cards.getOrDefault(x, 1) + cards.get(cardNumber)));
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		for (final String line : input) {
+			final int cardNumber = Integer.parseInt(line.substring(5, line.indexOf(":")).trim());
+			cards.putIfAbsent(cardNumber, 1);
+			final Set<String> winningNumbers = Stream
+					.of(line.substring(line.indexOf(":"), line.indexOf("|")).trim().split("\\s+"))
+					.collect(Collectors.toSet());
+			final Set<String> ourNumbers = Stream
+					.of(line.substring(line.indexOf("|") + 1, line.length()).trim().split("\\s+"))
+					.collect(Collectors.toSet());
+			final int numMatches = (int) ourNumbers.stream().filter(x -> winningNumbers.contains(x)).count();
+			IntStream.range(cardNumber + 1, cardNumber + numMatches + 1)
+					.forEach(x -> cards.put(x, cards.getOrDefault(x, 1) + cards.get(cardNumber)));
 		}
 		return cards.values().stream().mapToInt(Integer::intValue).sum();
 	}
